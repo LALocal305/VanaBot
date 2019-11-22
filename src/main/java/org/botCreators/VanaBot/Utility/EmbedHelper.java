b090ec1547d8071090ad9ff722ffd3353353b664
Abraham <abeponce@gmail.com>
2019-11-22 13:56:23 -0800

    Added stats for server populations.

--- src/main/java/org/botCreators/VanaBot/Utility/EmbedHelper.java
+++ src/main/java/org/botCreators/VanaBot/Utility/EmbedHelper.java
@@ -52,7 +52,54 @@ public class EmbedHelper {
     	int aMembers = event.getGuild().getMembersWithRoles(event.getGuild().getRolesByName("Adoulin", true)).size();
     	sb.append("Adoulin citizens: **").append(aMembers).append("**\n");
     	
+    	sb.append("\nServer Population\n\n");
+    	int asuraMembers = event.getGuild().getMembersWithRoles(event.getGuild().getRolesByName("Asura", true)).size();
+    	sb.append("Asura: **").append(asuraMembers).append("**\n");
     	
+    	int bahamutMembers = event.getGuild().getMembersWithRoles(event.getGuild().getRolesByName("Bahamut", true)).size();
+    	sb.append("Bahamut: **").append(bahamutMembers).append("**\n");
+
+    	int bismarckMembers = event.getGuild().getMembersWithRoles(event.getGuild().getRolesByName("Bismarck", true)).size();
+    	sb.append("Bismarck: **").append(bismarckMembers).append("**\n");
+    	
+    	int carbuncleMembers = event.getGuild().getMembersWithRoles(event.getGuild().getRolesByName("Carbuncle", true)).size();
+    	sb.append("Carbuncle: **").append(carbuncleMembers).append("**\n");
+    	
+    	int cerberusMembers = event.getGuild().getMembersWithRoles(event.getGuild().getRolesByName("Cerberus", true)).size();
+    	sb.append("Cerberus: **").append(cerberusMembers).append("**\n");
+    	
+    	int fenrirMembers = event.getGuild().getMembersWithRoles(event.getGuild().getRolesByName("Fenrir", true)).size();
+    	sb.append("Fenrir: **").append(fenrirMembers).append("**\n");
+    	
+    	int lakshmiMembers = event.getGuild().getMembersWithRoles(event.getGuild().getRolesByName("Lakshmi", true)).size();
+    	sb.append("Lakshmi: **").append(lakshmiMembers).append("**\n");
+    	
+    	int leviathanMembers = event.getGuild().getMembersWithRoles(event.getGuild().getRolesByName("Leviathan", true)).size();
+    	sb.append("Leviathan: **").append(leviathanMembers).append("**\n");
+    	
+    	int odinMembers = event.getGuild().getMembersWithRoles(event.getGuild().getRolesByName("Odin", true)).size();
+    	sb.append("Odin: **").append(odinMembers).append("**\n");
+    	
+    	int phoenixMembers = event.getGuild().getMembersWithRoles(event.getGuild().getRolesByName("Phoenix", true)).size();
+    	sb.append("Phoenix: **").append(phoenixMembers).append("**\n");
+    	
+    	int quetzMembers = event.getGuild().getMembersWithRoles(event.getGuild().getRolesByName("Quetzalcoatl", true)).size();
+    	sb.append("Quetzalcoatl: **").append(quetzMembers).append("**\n");
+    	
+    	int ragnarokMembers = event.getGuild().getMembersWithRoles(event.getGuild().getRolesByName("Ragnarok", true)).size();
+    	sb.append("Ragnarok: **").append(ragnarokMembers).append("**\n");
+    	
+    	int shivaMembers = event.getGuild().getMembersWithRoles(event.getGuild().getRolesByName("Shiva", true)).size();
+    	sb.append("Shiva: **").append(shivaMembers).append("**\n");
+    	
+    	int sirenMembers = event.getGuild().getMembersWithRoles(event.getGuild().getRolesByName("Siren", true)).size();
+    	sb.append("Siren: **").append(sirenMembers).append("**\n");
+    	
+    	int sylphMembers = event.getGuild().getMembersWithRoles(event.getGuild().getRolesByName("Sylph", true)).size();
+    	sb.append("Sylph: **").append(sylphMembers).append("**\n");
+    	
+    	int valeforMembers = event.getGuild().getMembersWithRoles(event.getGuild().getRolesByName("Valefor", true)).size();
+    	sb.append("Valefor: **").append(valeforMembers).append("**\n");
     	
     	
     	embed.addField("Server Stats", sb.toString(), false);
